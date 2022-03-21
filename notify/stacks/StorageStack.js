bd5e8d96437825b567dbb0bde89a4083c664d08d
satish <satish860@gmail.com>
2022-03-21 11:16:56 +0530

    Created the S3 bucket to upload file

--- notify/stacks/StorageStack.js
+++ notify/stacks/StorageStack.js
@@ -1,17 +1,31 @@
 import * as sst from "@serverless-stack/resources";
 
-export default class StorageStack extends sst.Stack{
+export default class StorageStack extends sst.Stack {
     table;
+    bucket;
 
-    constructor(scope,id,props){
-        super(scope,id,props)
+    constructor(scope, id, props) {
+        super(scope, id, props)
 
-        this.table = new sst.Table(this,"Templates",{
+        this.table = new sst.Table(this, "Templates", {
             fields: {
-                user_Id : sst.TableFieldType.STRING,
-                template_id : sst.TableFieldType.STRING
+                user_Id: sst.TableFieldType.STRING,
+                template_id: sst.TableFieldType.STRING
             },
-            primaryIndex: {partitionKey:"user_Id",sortKey:"template_id"}
-        })
+            primaryIndex: { partitionKey: "user_Id", sortKey: "template_id" }
+        });
+
+        this.bucket = new sst.Bucket(this, "uploads", {
+            s3Bucket: {
+                cors: [
+                    {
+                        maxAge: 3000,
+                        allowedOrigins: ["*"],
+                        allowedHeaders: ["*"],
+                        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"]
+                    },
+                ],
+            },
+        });
     }
 }
