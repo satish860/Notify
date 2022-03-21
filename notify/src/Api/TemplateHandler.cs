5b6621b52dc0bdfd60c8ce1514fc7294b9a5a8aa
satish <satish860@gmail.com>
2022-03-21 10:11:48 +0530

    Removed the repeated DynamoDB operation config out

--- notify/src/Api/TemplateHandler.cs
+++ notify/src/Api/TemplateHandler.cs
@@ -13,17 +13,20 @@ namespace Api
     public class TemplateHandler
     {
         private readonly AmazonDynamoDBClient client;
+        private readonly DynamoDBOperationConfig operationConfig;
         public TemplateHandler()
         {
             client = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWS_REGION")));
+            operationConfig = new DynamoDBOperationConfig
+            {
+                OverrideTableName = Environment.GetEnvironmentVariable("TABLE_NAME")
+            };
         }
-       
+
         public async Task<APIGatewayHttpApiV2ProxyResponse> CreateTemplates(APIGatewayHttpApiV2ProxyRequest request)
         {
-
             var serializer = JsonSerializer.Deserialize<Template>(request.Body);
             DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
-            var TableName = Environment.GetEnvironmentVariable("TABLE_NAME");
             Template template = new Template
             {
                 TemplateId = Guid.NewGuid().ToString(),
@@ -31,10 +34,7 @@ namespace Api
                 Name = serializer.Name,
                 Text = serializer.Text,
             };
-            await dynamoDBContext.SaveAsync(template, new DynamoDBOperationConfig
-            {
-                OverrideTableName = TableName,
-            });
+            await dynamoDBContext.SaveAsync(template, operationConfig);
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
@@ -47,11 +47,7 @@ namespace Api
         {
             var template = JsonSerializer.Deserialize<Template>(request.Body);
             DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
-            var TableName = Environment.GetEnvironmentVariable("TABLE_NAME");
-            await dynamoDBContext.SaveAsync(template, new DynamoDBOperationConfig
-            {
-                OverrideTableName = TableName,
-            });
+            await dynamoDBContext.SaveAsync(template, operationConfig);
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.Accepted,
@@ -65,16 +61,12 @@ namespace Api
             var userId = request.PathParameters["user_id"];
             var templateId = request.PathParameters["template_id"];
             DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
-            var TableName = Environment.GetEnvironmentVariable("TABLE_NAME");
             Template template = new Template
             {
                 UserId = userId,
                 TemplateId = templateId,
             };
-            dynamoDBContext.DeleteAsync(template, new DynamoDBOperationConfig
-            {
-                OverrideTableName = TableName,
-            });
+            dynamoDBContext.DeleteAsync(template,operationConfig);
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.NoContent,
@@ -86,16 +78,12 @@ namespace Api
             var userId = request.PathParameters["user_id"];
             var templateId = request.PathParameters["template_id"];
             DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
-            var TableName = Environment.GetEnvironmentVariable("TABLE_NAME");
             Template template = new Template
             {
                 UserId = userId,
                 TemplateId = templateId,
             };
-           var templateDetails = await dynamoDBContext.LoadAsync<Template>(template, new DynamoDBOperationConfig
-            {
-                OverrideTableName = TableName,
-            });
+            var templateDetails = await dynamoDBContext.LoadAsync(template, operationConfig);
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
@@ -108,11 +96,7 @@ namespace Api
         {
             var userId = request.PathParameters["user_id"];
             DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
-            var TableName = Environment.GetEnvironmentVariable("TABLE_NAME");
-            var searchResult = dynamoDBContext.QueryAsync<Template>(userId, new DynamoDBOperationConfig
-            {
-                OverrideTableName = TableName,
-            });
+            var searchResult = dynamoDBContext.QueryAsync<Template>(userId, operationConfig);
             var templates = new List<Template>();
             do
             {
