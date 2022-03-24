8ba1c681a3cc9d4083cb7d64f091dac112550862
satish <satish860@gmail.com>
2022-03-24 14:42:30 +0530

    Create Notification.cs

---
+++ notify/src/Api/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api
+{
+    public class Notification
+    {
+        public string Message { get; set; }
+
+        public string MessageTemplateId { get; set; }
+
+        public string RecipientListFile { get; set; }
+
+        public string UserId { get; set; }
+
+        public string NotificationId { get; set; }
+    }
+}
