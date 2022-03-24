9433e24c770adc32225eaaed509277e5fb58372e
satish <satish860@gmail.com>
2022-03-24 16:40:58 +0530

    Create Notification Handler with the DynamoDB

--- notify/src/Api/Notification.cs
+++ notify/src/Api/Notification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,22 @@ namespace Api
 {
     public class Notification
     {
+        [DynamoDBProperty("message")]
         public string Message { get; set; }
 
+        [DynamoDBProperty("message_template_id")]
         public string MessageTemplateId { get; set; }
 
+        [DynamoDBProperty("recipent_list_file")]
         public string RecipientListFile { get; set; }
 
+        [DynamoDBProperty("recipent")]
+        public string Recipent { get; set; }
+
+        [DynamoDBHashKey("user_Id")]
         public string UserId { get; set; }
 
+        [DynamoDBRangeKey("notification_Id")]
         public string NotificationId { get; set; }
     }
 }
--- notify/src/Api/NotificationHandler.cs
+++ notify/src/Api/NotificationHandler.cs
@@ -1,20 +1,38 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Api
 {
     public class NotificationHandler
     {
+        private readonly AmazonDynamoDBClient client;
+        private readonly DynamoDBOperationConfig operationConfig;
+        public NotificationHandler()
+        {
+            client = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWS_REGION")));
+            operationConfig = new DynamoDBOperationConfig
+            {
+                OverrideTableName = Environment.GetEnvironmentVariable("NOTIFICATION_TABLE")
+            };
+        }
         public async Task<APIGatewayHttpApiV2ProxyResponse> CreateNotification(APIGatewayHttpApiV2ProxyRequest request)
         {
+            var notification = JsonSerializer.Deserialize<Notification>(request.Body);
+            DynamoDBContext dynamoDBContext = new DynamoDBContext(client);
+            notification.NotificationId = Guid.NewGuid().ToString();
+            await dynamoDBContext.SaveAsync(notification, operationConfig);
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
-                Body = $"Create Notification for Message - {request.RequestContext.Time}.",
+                Body = JsonSerializer.Serialize(notification),
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
         }
